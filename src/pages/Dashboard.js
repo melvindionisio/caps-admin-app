5c3c328e86f2f6b1df355d9805b579cb22d4a110
Melvin Dionisio <75345253+melvindionisio@users.noreply.github.com>
2021-11-27 12:56:48 +0800

    profile page added

--- src/pages/Dashboard.js
+++ src/pages/Dashboard.js
@@ -4,11 +4,31 @@ import { Container, Typography, Box } from "@mui/material";
 
 const Dashboard = () => {
   return (
-    <Container maxWidth="xl" disableGutters>
+    <Container maxWidth="xl" disableGutters sx={{ minHeight: "100vh" }}>
       <HomeNavigation title="dashboard" />
       <Box sx={{ p: 2 }}>
         <Typography variant="body2" color="initial">
           Total Bording house, boarding house by brgy,
+        </Typography>
+        <Typography variant="h1" color="initial">
+          Lorem ipsum, dolor sit amet consectetur adipisicing elit. Atque eum
+          aperiam consequuntur eaque alias perspiciatis odit. Consectetur
+          perspiciatis amet aperiam voluptas, unde laborum placeat? Molestias
+          sit necessitatibus perspiciatis corrupti magni! Quam molestias
+          eligendi modi corporis praesentium minus ipsum expedita deleniti
+          doloremque labore adipisci, reiciendis voluptate rerum eum debitis nam
+          laudantium quaerat illum deserunt natus! Alias temporibus aliquid
+          maxime odio adipisci? Inventore recusandae excepturi quod delectus
+          porro ipsum culpa impedit voluptates molestias officia maiores,
+          veritatis voluptatem ab fugit, enim consectetur pariatur laboriosam
+          natus nesciunt rerum. Neque provident officiis eveniet repellat minus.
+          Saepe quae velit eum temporibus ipsa, in non corrupti magni illo ut
+          nemo impedit atque praesentium placeat dolorem, veniam eveniet
+          deleniti recusandae. Natus non cumque eius, beatae autem explicabo
+          ducimus! Aspernatur esse molestias porro tempore fugit, incidunt quas
+          expedita corrupti, quisquam, libero obcaecati odio harum ullam ea amet
+          ut! Cum assumenda quisquam odit corporis temporibus porro fuga quas.
+          Molestiae, ipsum?
         </Typography>
       </Box>
     </Container>
--- src/pages/Profile.js
+++ src/pages/Profile.js
@@ -1,16 +1,214 @@
 import React from "react";
 import HomeNavigation from "../components/HomeNavigation";
-import { Container, Typography, Box } from "@mui/material";
+import {
+  Container,
+  Box,
+  Chip,
+  CardHeader,
+  Avatar,
+  Card,
+  IconButton,
+  TextField,
+  Button,
+  Alert,
+} from "@mui/material";
+import { EditOutlined } from "@mui/icons-material";
+import { useState } from "react";
+import { CardContent, Divider } from "@material-ui/core";
+import { blue, grey } from "@material-ui/core/colors";
 
 const Profile = () => {
+  const [name, setName] = useState("Melvin Dionisio");
+  const [userName, setUserName] = useState("melsio021");
+  const [password, setPassword] = useState("password");
+  const [newPassword, setNewPassword] = useState("");
+  const [curPassword, setCurPassword] = useState("");
+  const [rePassword, setRePassword] = useState("");
+
+  const [profileEditable, setProfileEditable] = useState(false);
+  const [isChangePassword, setIsChangePassword] = useState(false);
+  const [showAlert, setShowAlert] = useState(false);
+  const [alertMessage, setAlertMessage] = useState("");
+
+  // CHANGE NAME/USERNAME REQUEST
+  // const changeUserName = () => {
+  //   let profile = {
+  //     name: name,
+  //     username: userName,
+  //   };
+  // };
+
+  // CHANGE PASSWORD REQUEST
+  const changePassword = () => {
+    let changePasswordContent = {};
+    if (curPassword && newPassword && rePassword !== "") {
+      if (password === curPassword) {
+        changePasswordContent = {
+          currentPassword: curPassword,
+          newPassword: newPassword,
+          repeatNewPassword: rePassword,
+        };
+        setIsChangePassword(!isChangePassword);
+        setNewPassword("");
+        setCurPassword("");
+        setRePassword("");
+      } else {
+        setAlertMessage("Current Password Incorrect!");
+        setShowAlert(true);
+      }
+    } else {
+      setAlertMessage("Please fill the field!");
+      setShowAlert(true);
+    }
+  };
+
   return (
-    <Container maxWidth="xl" disableGutters>
+    <Container maxWidth="xl" disableGutters sx={{ minHeight: "100vh" }}>
       <HomeNavigation title="profile" />
-      <Box sx={{ p: 2 }}>
-        <Typography variant="body2" color="initial">
-          Profile
-        </Typography>
-      </Box>
+      <Container maxWidth="sm" sx={{ p: 2, display: "flex" }}>
+        <Card sx={{ width: "90%", p: 2, margin: "0 auto" }}>
+          <CardHeader
+            avatar={<Avatar aria-label="profile-pic"></Avatar>}
+            action={
+              !profileEditable ? (
+                <IconButton
+                  aria-label="edit-icon"
+                  onClick={() => setProfileEditable(!profileEditable)}
+                >
+                  <EditOutlined />
+                </IconButton>
+              ) : (
+                <Button
+                  size="small"
+                  variant="contained"
+                  disableElevation
+                  onClick={() => setProfileEditable(!profileEditable)}
+                >
+                  save
+                </Button>
+              )
+            }
+            title="You are Login as: "
+            subheader="Admin"
+          />
+          <CardContent>
+            <Chip size="small" label="PROFILE" />
+
+            <TextField
+              id="name"
+              label="Name"
+              value={name}
+              onChange={(e) => setName(e.target.value)}
+              fullWidth
+              size="small"
+              variant="outlined"
+              margin="dense"
+              disabled={!profileEditable}
+            />
+            <TextField
+              id="username"
+              label="Username"
+              value={userName}
+              onChange={(e) => setUserName(e.target.value)}
+              fullWidth
+              size="small"
+              variant="outlined"
+              margin="dense"
+              disabled={!profileEditable}
+            />
+            <TextField
+              id="password"
+              label="Password"
+              type="password"
+              value={password}
+              onChange={(e) => setPassword(e.target.value)}
+              fullWidth
+              size="small"
+              variant="outlined"
+              margin="dense"
+              disabled
+              sx={{ mb: 3 }}
+            />
+
+            <Box
+              sx={{
+                display: "flex",
+                justifyContent: "space-between",
+                alignItems: "center",
+                height: 13,
+                mb: 1,
+              }}
+            >
+              <Chip size="small" label="PASSWORD" />
+              {!isChangePassword ? (
+                <IconButton
+                  aria-label="edit-icon"
+                  onClick={() => setIsChangePassword(!isChangePassword)}
+                >
+                  <EditOutlined />
+                </IconButton>
+              ) : (
+                <Button
+                  size="small"
+                  variant="contained"
+                  disableElevation
+                  onClick={changePassword}
+                >
+                  change password
+                </Button>
+              )}
+            </Box>
+            <Box>
+              <TextField
+                id="cur-password"
+                label="Current Password"
+                type="password"
+                value={curPassword}
+                onChange={(e) => setCurPassword(e.target.value)}
+                fullWidth
+                size="small"
+                variant="outlined"
+                margin="dense"
+                disabled={!isChangePassword}
+              />
+              <TextField
+                id="password"
+                label="New Password"
+                type="password"
+                value={newPassword}
+                onChange={(e) => setNewPassword(e.target.value)}
+                fullWidth
+                size="small"
+                variant="outlined"
+                margin="dense"
+                disabled={!isChangePassword}
+              />
+              <TextField
+                id="re-password"
+                label="Repeat Password"
+                type="password"
+                value={rePassword}
+                onChange={(e) => setRePassword(e.target.value)}
+                fullWidth
+                size="small"
+                variant="outlined"
+                margin="dense"
+                disabled={!isChangePassword}
+              />
+            </Box>
+            <Alert
+              severity="error"
+              sx={
+                showAlert
+                  ? { display: "flex", mt: 2 }
+                  : { display: "none", mt: 2 }
+              }
+            >
+              {alertMessage}
+            </Alert>
+          </CardContent>
+        </Card>
+      </Container>
     </Container>
   );
 };
